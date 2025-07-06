9a6352677aabf932c52937179cdff8fcbb9e8b1c
Lunaticus777 <vitor.velho@av-imo.com>
2025-07-07 00:51:51 +0100

    Correctifs sur la section Home + mise à jour contenu

--- Models/ContactViewModel.cs
+++ Models/ContactViewModel.cs
@@ -11,8 +11,11 @@ namespace Human_Evolution.Models
         [EmailAddress(ErrorMessage = "Adresse email invalide.")]
         public string Email { get; set; }
 
+        [Phone(ErrorMessage = "Numéro de téléphone invalide.")]
+        [Required(ErrorMessage = "Le téléphone est requis.")] // ← tu peux enlever cette ligne si ce n’est pas obligatoire
+        public string Phone { get; set; }
+
         [Required(ErrorMessage = "Le message est requis.")]
         public string Message { get; set; }
     }
 }
-

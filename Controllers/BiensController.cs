795e7e70d52f0cc777b3cdae4f4b1f8c61069856
Lunaticus777 <vitor.velho@av-imo.com>
2026-05-11 00:17:18 +0100

    Update Human Square branding

---
+++ Controllers/BiensController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Evolution.Controllers
+{
+    public class BiensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

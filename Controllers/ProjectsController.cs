684c9a940dd424ae778201e4dfa08ff2bb00216f
Lunaticus777 <vitor.velho@av-imo.com>
2026-05-14 17:07:36 +0100

    Update projects page and carousels

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,6 +1,5 @@
 ﻿using Human_Evolution.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 
 namespace Human_Evolution.Controllers
 {
@@ -10,29 +9,134 @@ namespace Human_Evolution.Controllers
         {
             ViewBag.ActiveTab = "clients";
 
-            var projetsClients = new List<Project>
-            {
-                new Project { Title = "7 Villas à Esposende", ImageUrl = "/images/project01b.png", Description = "Projet en bord de mer", ModalId = "modalProjet1" },
-                new Project { Title = "10 Villas à Albufeira", ImageUrl = "/images/project02i.jpg", Description = "Projet dans le sud", ModalId = "modalProjet2" },
-                new Project { Title = "Villa à Barcelos", ImageUrl = "/images/project03.png", Description = "Projet individuel", ModalId = "modalProjet3" },
-                new Project { Title = "6 Villas à Viseu", ImageUrl = "/images/project04.jpg", Description = "Projet de montagne", ModalId = "modalProjet5" },
-                new Project { Title = "Projet immobilier touristique – Douro", ImageUrl = "/images/project06.jpg", Description = "Projet de réhabilitation", ModalId = "modalProjet6" }
-            };
+            var projetsClients = GetClientProjects();
 
-            return View("Index", projetsClients); // Utilise la même vue que Index
+            return View("Index", projetsClients);
         }
 
         public IActionResult Disponibles()
         {
             ViewBag.ActiveTab = "disponibles";
 
-            var projetsDispo = new List<Project>
-            {
-                new Project { Title = "Ruine à réhabiliter – Douro", ImageUrl = "/images/project06.jpg", Description = "Opportunité d’investissement", ModalId = "modalProjet6" },
-                new Project { Title = "Terrains à bâtir – Braga", ImageUrl = "/images/project07.jpg", Description = "Projet prêt à développer", ModalId = "modalProjet7" }
-            };
+            var projetsDisponibles = GetAvailableProjects();
 
-            return View(projetsDispo); // Affiche la vue Disponibles.cshtml
+            return View("Index", projetsDisponibles);
+        }
+
+        // =========================
+        // PROJETS CLIENTS
+        // =========================
+
+        private List<Project> GetClientProjects()
+        {
+            return new List<Project>
+            {
+                new Project
+                {
+                    Title = "7 Villas à Esposende",
+                    Description = "Projet en bord de mer",
+                    ImageUrl = "/images/project01b.png",
+                    ModalId = "modalProjet1"
+                },
+
+                new Project
+                {
+                    Title = "10 Villas à Albufeira",
+                    Description = "Projet dans le sud",
+                    ImageUrl = "/images/project02i.jpg",
+                    ModalId = "modalProjet2"
+                },
+
+                new Project
+                {
+                    Title = "Villa à Barcelos",
+                    Description = "Projet individuel",
+                    ImageUrl = "/images/barcelos.jpeg",
+                    ModalId = "modalProjet3"
+                },
+
+                new Project
+                {
+                    Title = "6 Villas à Viseu",
+                    Description = "Projet de montagne",
+                    ImageUrl = "/images/project04.jpg",
+                    ModalId = "modalProjet5"
+                },
+
+                new Project
+                {
+                    Title = "Projet immobilier touristique – Douro",
+                    Description = "Projet de réhabilitation",
+                    ImageUrl = "/images/project06.jpg",
+                    ModalId = "modalProjet6"
+                },
+
+                new Project
+                {
+                    Title = "Projet touristique Lomba – Gondomar",
+                    Description = "Projet touristique avec PIP approuvé",
+                    ImageUrl = "/images/lomba-01.png",
+                    ModalId = "modalLomba"
+                },
+
+                new Project
+                {
+                    Title = "15 appartements à Joane",
+                    Description = "Projet résidentiel moderne",
+                    ImageUrl = "/images/joane.jpeg",
+                    ModalId = "modalProjet7"
+                },
+
+                new Project
+                {
+                    Title = "15 villas à Porto",
+                    Description = "Projet haut standing à Gaia",
+                    ImageUrl = "/images/gaia.jpg",
+                    ModalId = "modalProjet8"
+                }
+            };
+        }
+
+        // =========================
+        // PROJETS DISPONIBLES
+        // =========================
+
+        private List<Project> GetAvailableProjects()
+        {
+            return new List<Project>
+            {
+                new Project
+                {
+                    Title = "Ruine à réhabiliter – Douro",
+                    Description = "Opportunité d’investissement",
+                    ImageUrl = "/images/project06.jpg",
+                    ModalId = "modalProjet6"
+                },
+
+                new Project
+                {
+                    Title = "Projet touristique Lomba – Gondomar",
+                    Description = "Projet avec PIP approuvé",
+                    ImageUrl = "/images/lomba-01.png",
+                    ModalId = "modalLomba"
+                },
+
+                new Project
+                {
+                    Title = "15 appartements à Joane",
+                    Description = "Projet prêt à développer",
+                    ImageUrl = "/images/joane.jpeg",
+                    ModalId = "modalProjet7"
+                },
+
+                new Project
+                {
+                    Title = "15 villas à Porto",
+                    Description = "Projet résidentiel premium",
+                    ImageUrl = "/images/gaia.jpg",
+                    ModalId = "modalProjet8"
+                }
+            };
         }
     }
 }
